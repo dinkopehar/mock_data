a201c43e3422895c9af2c5a1875c65cd644f4a21
PinkFrojd <dinko.pehar.dev@gmail.com>
2018-12-17 23:52:03 +0100

    Added additional tests for all mock functions

--- test/mock_data_test.dart
+++ test/mock_data_test.dart
@@ -6,8 +6,8 @@ void main() {
   group('mockString tests', () {
 
     test('Test mockString only', () {
-      expect(mockString().length, 16);
-      expect(mockString(5).length, 5);
+      expect(mockString().length, equals(16));
+      expect(mockString(5).length, equals(5));
       //expect(mockString(16, 'aA').contains(RegExp(r'(\d)')), isFalse);
       //expect(mockString(16, '!').contains(RegExp(r'(\d)')), isTrue);
       //expect(mockString(7, 'aA').contains(RegExp(r'[\w]')), isTrue);
@@ -17,32 +17,40 @@ void main() {
     });
     
     test('Test mockString called from mockRange', () {
-      expect(mockRange(mockString, 10).length, 10);
+      expect(mockRange(mockString, 10).length, equals(10));
       expect(mockRange(mockString, 5,
           lengthOfMockedString: 3).map((s) => s.length).toList(),
-          List<int>.generate(5, (_) => 3));
+          equals(List<int>.generate(5, (_) => 3)));
     });
   });
 
   group('mockInteger tests', () {
 
     test('Test mockInteger only', () {
-
+      expect(mockInteger(), inInclusiveRange(1, 10));
+      expect(mockInteger(-15, -10), inInclusiveRange(-15, -10));
+      expect(mockInteger(-7, 17), inInclusiveRange(-7, 17));
+      mockRange(mockInteger, 100, min: 1, max: 10).forEach((el) =>
+          expect(el, inInclusiveRange(1, 10)));
     });
 
     test('Test mockInteger called from mockRange', () {
-
+      mockRange(mockInteger, 100, min: 1, max: 10).forEach((el) =>
+          expect(el, inInclusiveRange(1, 10)));
     });
+
   });
 
   group('mockIPv4 tests', () {
 
     test('Test mockIPv4 only', () {
-
+      expect(mockIPv4('192.168.0.1'), equals('192.168.0.1'));
+      expect(mockIPv4('192.168.*.*'), startsWith('192.168.'));
+      expect(mockIPv4('*.168.*.*').split('.').length, equals(4));
     });
 
     test('Test mockIPv4 called from mockRange', () {
-
+      //mockRange(mockIPv4, 15).forEach((ip) => expect(ip.toString(), ));
     });
   });
 
@@ -55,7 +63,7 @@ void main() {
     });
 
     test('Test mockName called from mockRange', () {
-      expect(mockRange(mockName, 15).length, 15);
+      expect(mockRange(mockName, 15).length, equals(15));
       expect(femaleNames, containsAll(
           mockRange(mockName, 15, gender: 'female').toSet()));
     });
